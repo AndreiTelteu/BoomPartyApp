b7f9cd664134bee4496516cb59b762a81f20a314
Andrei Telteu <andrei.telteu@touch-media.ro>
2021-08-21 19:42:53 +0300

    Uninstalled bluetooth-cross-platform plugin. can't work

--- android/app/src/main/java/com/boomparty/MainApplication.java
+++ android/app/src/main/java/com/boomparty/MainApplication.java
@@ -8,7 +8,6 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.rctunderdark.NetworkManagerPackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new NetworkManagerPackage());
           return packages;
         }
 
